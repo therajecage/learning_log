5c80204a96b4231a82f075528ec69646fd4c0865
Manish Raje <raje.manish@gmail.com>
2021-03-13 19:04:42 +0100

    Ready for deployment to Heroku

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from  django.conf.urls import url
+from django.urls import path
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [ path('login/', LoginView.as_view( template_name='users/login.html'), name='login'),
+
+				path('logout/', views.logout_view, name='logout'),
+
+				path('register/', views.register, name='register')
+
+
+	]
